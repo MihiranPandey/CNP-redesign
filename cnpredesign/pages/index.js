04bf5119ae4c7ceb3817d8a52bf1690b7e41747e
mihiranpandey <mihiran.pandey@credera.com>
2022-07-18 17:21:52 -0500

    removed the reference to home.modules.css

--- cnpredesign/pages/index.js
+++ cnpredesign/pages/index.js
@@ -1,11 +1,10 @@
 import Head, { defaultHead } from 'next/head'
 import Image from 'next/image'
 import Navbar from '../components/Navbar'
-import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
 
       <Head>
         <link rel="stylesheet" href="https://use.typekit.net/ynv4tav.css"/>
