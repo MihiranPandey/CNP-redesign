dbfadfaa65b5ba3c29a01878b1102eb0e9b5776f
mihiranpandey <mihiran.pandey@credera.com>
2022-07-18 16:05:59 -0500

    updated Navbar for more responsiveness and a few changes from new design

--- cnpredesign/components/Navbar.jsx
+++ cnpredesign/components/Navbar.jsx
@@ -3,10 +3,9 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from './Navbar.module.css'
 import logo from '../public/assets/cnp.svg'
-import adown from '../public/assets/arrow-down.svg'
-import adownw from '../public/assets/arrow-downw.svg'
-import acc from '../public/assets/Account.svg'
-import map from '../public/assets/map-pin.svg'
+import arrow_down from '../public/assets/arrow-down.svg'
+import account from '../public/assets/account.svg'
+import map_pin from '../public/assets/map-pin.svg'
 import search from '../public/assets/search.svg'
 
 
@@ -15,42 +14,46 @@ export default function Navbar() {
         <div className={styles.navigation_bar}>
             <div className={styles.left_nav_section}>
                 <div className={styles.logo}>
-                    <Image src={logo} alt="logo" width='235px' height='82px'/>
+                    <Image src={logo} alt="logo" width='235px' height='82px' layout='fixed'/>
                 </div>
-                <div className={styles.version_type}>
-                    <p>Residential</p>
-                    <Image src={adownw} alt="down arrow" width='24px' height='24px'/>
+                <div className={styles.divider}>
+                    <div className={styles.version_type}>
+                        Residential
+                        <Image className={styles.pic} src={arrow_down} alt="down arrow" width='24px' height='24px' layout='fixed'/>
+                    </div>
                 </div>
             </div>
 
             <div className={styles.right_nav_section}>
                 <div className={styles.nav_links}>
                     <div className={styles.nav_links}>
-                        <a className={styles.link}>
+                        <div className={styles.link}>
                         Explore
-                        </a>
-                        <Image src={adown} alt="down arrow" width='24px' height='24px'/>
+                        </div>
+                        <Image src={arrow_down} alt="down arrow" width='24px' height='24px' layout='fixed'/>
                     </div>
                     
                     <div className={styles.nav_links}>
-                        <a className={styles.link}>
+                        <div className={styles.link}>
                             Contact
-                        </a>
-                        <Image src={adown} alt="down arrow" width='24px' height='24px'/>
+                        </div>
+                        <Image src={arrow_down} alt="down arrow" width='24px' height='24px' layout='fixed'/>
                     </div>
 
                     <div className={styles.nav_links}>
-                        <a className={styles.link}>
+                        <div className={styles.link}>
                             My Account
-                        </a>
-                        <Image src={adown} alt="down arrow" width='26px' height='26px'/>
+                        </div>
+                        <Image src={arrow_down} alt="down arrow" width='24px' height='24px' layout='fixed'/>
                     </div>
-                    <Image src={search} alt="search icon" width='30px' height='30px'/>
+
+                    <Image src={search} alt="search icon" width='30px' height='30px' layout='fixed'/>
+                    
                 </div>
-                <Image src={acc} alt="account icon" width='30px' height='30px'/>
+                <Image src={account} alt="account icon" width='30px' height='30px' layout='fixed'/>
                 <div className={styles.location}>
-                    <Image src={map} alt="map-pin icon" width='32px' height='24px'/>
-                    <a className={styles.link}> Houston </a> 
+                    <Image src={map_pin} alt="map_pin-pin icon" width='32px' height='24px' layout='fixed'/>
+                    <div className={styles.link}> Houston </div> 
                 </div>
             </div>
         </div>
